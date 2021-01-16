1048a8c75242546a1d29ecb92c6ea67d426b758e
Musab Guma'a <musabgumaa@gmail.com>
2021-01-16 09:22:12 +0000

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "main.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                exclude: "/node_modules/",
+                use: {
+                    loader: "awesome-typescript-loader"
+                }
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ],
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    }
+}
