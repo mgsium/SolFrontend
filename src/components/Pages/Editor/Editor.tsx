f87757346761508c05d18a82f2fb90b5ffeca815
Musab Guma'a <musabgumaa@gmail.com>
2021-01-17 01:15:00 +0000

    Added ability to create questions.

--- src/components/Pages/Editor/Editor.tsx
+++ src/components/Pages/Editor/Editor.tsx
@@ -16,12 +16,15 @@ import ReactPlayer from "react-player"
 import InitialDetailPage from "./InitialDetailPage/InitialDetailPage";
 import VideoSelectionPage from "./VideoSelectionPage.tsx/VideoSelectionPage";
 import LessonBuilder from "./LessonBuilder";
+import QuestionCreationPage from "./QuestionCreationPage/QuestionCreationPage";
+import Question from "../../../types/Question";
 
 type Props = {};
 type State = {
     showingLessonPage: boolean,
     currentPageIndex: 0 | 1 | 2 ,
-    lessonBuilder: LessonBuilder
+    lessonBuilder: LessonBuilder,
+    questions: Array<Question>
 };
 
 
@@ -33,13 +36,15 @@ export default class Editor extends React.Component<Props, State> {
         this.state = {
             lessonBuilder: new LessonBuilder(),
             showingLessonPage: false,
-            currentPageIndex: 0
+            currentPageIndex: 0,
+            questions: new Array<Question>()
         };
 
         // Method Bindings
         this.confirmInitialDetails = this.confirmInitialDetails.bind(this);
         this.confirmVideoUrl = this.confirmVideoUrl.bind(this);
         this.updatePageIndex = this.updatePageIndex.bind(this);
+        this.addQuestionToLesson = this.addQuestionToLesson.bind(this);
     }
 
     confirmInitialDetails(header: string, description: string, author_name: string, more_info: string) {
@@ -54,7 +59,15 @@ export default class Editor extends React.Component<Props, State> {
     confirmVideoUrl(url: string) {
         const lessonBuilder: LessonBuilder = this.state.lessonBuilder;
         lessonBuilder.setVideoUrl(url);
-        this.setState({ lessonBuilder: lessonBuilder });
+        this.setState({ lessonBuilder: lessonBuilder }, () => {
+            console.log(this.state.lessonBuilder.getLesson());
+        });
+    }
+
+    addQuestionToLesson(question: Question) {
+        const questions: Array<Question> = this.state.questions;
+        questions.push(question);
+        this.setState({ questions: questions });
     }
 
     updatePageIndex(index: 0 | 1 | 2) {
@@ -73,6 +86,13 @@ export default class Editor extends React.Component<Props, State> {
                         currentPageIndex={this.state.currentPageIndex}
                         updatePageIndex={this.updatePageIndex}
                         confirmVideoUrl={this.confirmVideoUrl}
+                    />
+                    <QuestionCreationPage
+                        currentPageIndex={this.state.currentPageIndex}
+                        updatePageIndex={this.updatePageIndex}
+                        videoUrl={this.state.lessonBuilder.getVideoUrl()}
+                        addQuestionToLesson={this.addQuestionToLesson}
+                        questions={this.state.questions}
                     />
             </Template>
         )
