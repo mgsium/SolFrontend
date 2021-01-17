5c08f6f5ed82cbf72fdf947290a9f23e11b2cb6a
Musab Guma'a <musabgumaa@gmail.com>
2021-01-17 20:37:39 +0000

    Updated Navbar Styling

--- src/components/Pages/Template/TemplateNavbar/TemplateNavbar.tsx
+++ src/components/Pages/Template/TemplateNavbar/TemplateNavbar.tsx
@@ -8,13 +8,16 @@ type Props = {};
 
 const TemplateNavbar: React.FC<Props> = () => {
     return (
-        <Navbar variant="light" color="light" style={{ borderBottom: "1px solid #DCDCDC" }}>
-            <Navbar.Brand className={cx("ml-auto", "mr-auto")} style={{ fontFamily: "Open Sans, sans-serif", fontWeight: 800, fontSize: 32 }}>
-                Sol
-                &nbsp;&nbsp;
-                <img src="../../../../public/assets/img/svg/SolLogo.svg" alt="Sol Logo" width={48} style={{ verticalAlign: "top", marginTop: 2 }}/>
-            </Navbar.Brand>
-        </Navbar>
+        <>
+            <Navbar variant="light" color="light" style={{ borderBottom: "1px solid #DCDCDC", position: "fixed", top: 0, width: "100vw", background: "white", zIndex: 2 }}>
+                <Navbar.Brand className={cx("ml-auto", "mr-auto")} style={{ fontFamily: "Open Sans, sans-serif", fontWeight: 800, fontSize: 32 }}>
+                    Sol
+                    &nbsp;&nbsp;
+                    <img src="../../../../public/assets/img/svg/SolLogo.svg" alt="Sol Logo" width={48} style={{ verticalAlign: "top", marginTop: 2 }}/>
+                </Navbar.Brand>
+            </Navbar>
+            <div style={{ height: 75 }}></div>
+        </>
     )
 }
 
